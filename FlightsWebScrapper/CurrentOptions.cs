1bc7b9d2c716f1a2421e4895a9e2f3fdbdde6569
Johann Hernandez Armenteros <johann.hernandez@fisterraenergy.com>
2020-05-17 17:14:00 -0500

    Created the basic structure of command CommandLine parsing.

---
+++ FlightsWebScrapper/CurrentOptions.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightsWebScrapper
+{
+    [Verb("current",HelpText ="Obtiene los datos del dia actual")]
+    public class CurrentOptions
+    {
+        [Option('f', "flight",  Required = true, HelpText = "Flight code to track data.", SetName = "flight")]
+        public string Flight { get; set; }
+    }
+}
--- FlightsWebScrapper/Program.cs
+++ FlightsWebScrapper/Program.cs
@@ -1,17 +1,60 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CommandLine;
+using CommandLine.Text;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace FlightsWebScrapper
 {
     class Program
     {
+        private static Parser parser;
+        public static Parsed<CurrentOptions> parserResult;
         public static IConfiguration Configuration;
         static void Main(string[] args)
         {
-             Configuration = new ConfigurationBuilder()
+            parser = new CommandLine.Parser(with => with.HelpWriter = null);
+            var result = parser.ParseArguments<CurrentOptions>(args)
+                .MapResult(
+                    (CurrentOptions current) => Run(current),
+                    (err) => ErrorsHandler(err)
+                    );
+
+            Configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .Build();
+                .Build();            
+        }
+
+        private static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            var helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.AutoVersion = false;
+                h.AutoHelp = false;
+                h.AdditionalNewLineAfterOption = false;
+                h.Heading = "ETL de Facturacion 1.0.0-beta"; //change header
+                h.Copyright = "Copyright (c) 2020 FisterraEnergy.com"; //change copyright text
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e);
+            Console.WriteLine(helpText);
+        }
+
+        private static int Run(CurrentOptions options)
+        {
+            if (string.IsNullOrEmpty(options.Flight))
+            {
+
+            }
+
+            return 0;
+        }
+
+        private static int ErrorsHandler(IEnumerable<Error> errors)
+        {
+            return 1;
         }
     }
 }
